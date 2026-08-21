2ff6d60ec8103097bfe14ac1e6fdf54c8d8e2a24
Subhashini-13 <subhashinim.cse2025@citchennai.net>
2026-08-21 22:47:30 +0530

    Create windows_installation.c

---
+++ windows_installation.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    printf("Experiment 1: Installation of Windows Operating System\n");
+    printf("C programming is not required for this experiment.\n");
+    printf("Verification is performed using Windows system commands.\n");
+    return 0;
+}
