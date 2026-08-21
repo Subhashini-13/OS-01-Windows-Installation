47cbae36e456ddfb95e85a3a9d09b86e563cd7e9
Subhashini-13 <subhashinim.cse2025@citchennai.net>
2026-08-21 22:48:05 +0530

    Create windows_verification.sh

---
+++ windows_verification.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo "Windows Operating System Verification"
+echo "====================================="
+
+echo
+echo "Computer Name:"
+hostname
+
+echo
+echo "IP Configuration:"
+ipconfig
+
+echo
+echo "System Information:"
+systeminfo
